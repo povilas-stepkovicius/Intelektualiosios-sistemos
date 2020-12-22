6d16d4e9d9462783f8116796f748a3aab7790cd1
povilas-stepkovicius <76068484+povilas-stepkovicius@users.noreply.github.com>
2020-12-22 19:50:39 +0200

    Add files via upload

--- lab3.m
+++ lab3.m
@@ -1,6 +1,16 @@
 clc;
 clear; 
 close all;
-x = [0.1:1/22:1]; %įėjimas (įėjimas iš 20 skaičių vektorius X
+x = [0.1:1/22:1]; %įėjimas iš 20 skaičių, vektorius X
 yOut = (1 + 0.6 * sin(2*pi*x/0.7)) + 0.3 * sin(2*pi*x) / 2; %vienas išėjimas 
 plot(x,yOut); 
+
+% generate random initial values of w1, w2 and b
+w1 = randn(1);
+w2 = randn(1);
+b = randn(1);
+
+c1 = 0.18;
+c2 = 0.9;
+r1 = 0.8;
+r2 = 0.12;
